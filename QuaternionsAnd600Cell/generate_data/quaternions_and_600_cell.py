892f5ac7e3455291caefbfd76dcd03556c4a19c3
p6majo <p6majo@gmx.de>
2025-09-11 00:19:11 +0200

    20250910  quaternions_and_600_cell.py, cell detection towards equation

--- QuaternionsAnd600Cell/generate_data/quaternions_and_600_cell.py
+++ QuaternionsAnd600Cell/generate_data/quaternions_and_600_cell.py
@@ -7,8 +7,6 @@ import numpy as np
 # let's start with field extension Q[r5], which are the rational numbers and the  root of five adjoined
 
 class QR5:
-
-
     def __init__(self,x:Fraction,y:Fraction):
         """
         >>> QR5(Fraction(1, 2), Fraction(3, 4))
@@ -304,6 +302,100 @@ class FQuaternion:
     def to_vector(self):
         return [self.a.re,self.a.im,self.b.re,self.b.im]
 
+class FVector:
+    def __init__(self, components:list):
+        self.dim = len(components)
+        self.components = components
+
+    def dot(self,other):
+        return sum([a*b for a,b in zip(self.components,other.components)],start=QR5(Fraction(0,1),Fraction(0,1)))
+
+class FTensor:
+    def __init__(self,components):
+        self.components = components
+        self.rank = self.get_rank()
+        self.dims = self.get_dimensions()
+
+
+    def get_dimensions(self):
+        """
+        >>> FTensor([1,2,3]).dims
+        [3]
+
+        >>> FTensor([[[1,2],[3,4]],[[5,6],[7,8]]]).dims
+        [2, 2, 2]
+
+        >>> FTensor(5).dims
+        []
+
+        :return:
+        """
+        return self._get_dimensions(self.components)
+
+    def get_rank(self):
+        """
+        >>> FTensor([[1,2],[3,4]]).rank
+        2
+        >>> FTensor([[[1,2],[3,4]],[[5,6],[7,8]]]).rank
+        3
+        >>> FTensor(5).rank
+        0
+
+        :param components:
+        :return:
+        """
+        return self._rank_rec(self.components)
+
+    def _rank_rec(self,components):
+        if isinstance(components,list):
+            return self._rank_rec(components[0])+1
+        else:
+            return 0
+
+    def _get_dimensions(self,components):
+        if isinstance(components,list):
+            result = self._get_dimensions(components[0])
+            result.append(len(components))
+            return result
+        else:
+            return list()
+
+    def scale(self,alpha:QR5):
+        """
+        >>> np.random.seed(1234)
+        >>> comps = np.array([QR5.random() for i in range(12)])
+        >>> components = comps.reshape(3,4)
+
+        >>> tensor = FTensor(components)
+        >>> tensor.components.tolist()
+        [[(5+9*r5), (-4+2*r5), (5+7*r5), (-1+1*r5)], [(2+6*r5), (-5+6*r5), (-1+5*r5), (8+6*r5)], [(2-5*r5), (-8-4*r5), (-7-3*r5), (1-10*r5)]]
+
+        >>> tensor = tensor.scale(QR5(Fraction(2,1),Fraction(0,1)))
+        >>> tensor.components.tolist()
+        [[(10+18*r5), (-8+4*r5), (10+14*r5), (-2+2*r5)], [(4+12*r5), (-10+12*r5), (-2+10*r5), (16+12*r5)], [(4-10*r5), (-16-8*r5), (-14-6*r5), (2-20*r5)]]
+
+        :param alpha:
+        :return:
+        """
+        return FTensor(self.components*alpha)
+
+    def __mul__(self,other):
+        """
+        tensor product between two tensors
+        >>> np.random.seed(1234)
+        >>> comps = np.array([QR5.random() for i in range(12)])
+        >>> components = comps.reshape(3,4)
+        >>> comps2 = np.array([QR5.random() for i in range(12)])
+        >>> components2 = comps2.reshape(3,2,2)
+        >>> product = FTensor(components)*FTensor(components2)
+        >>> product.components.tolist()
+        [[[[[(40-4*r5), (-285+19*r5)], [(365-27*r5), (-395-27*r5)]], [[(410+54*r5), (320+44*r5)], [(425+81*r5), (-15-27*r5)]], [[(185+29*r5), (170+78*r5)], [(40-80*r5), (290-10*r5)]]], [[[(14-6*r5), (-94+40*r5)], [(122-52*r5), (-98+40*r5)]], [[(86-34*r5), (66-26*r5)], [(74-28*r5), (12-6*r5)]], [[(36-14*r5), (2+2*r5)], [(60-28*r5), (90-38*r5)]]], [[[(30-2*r5), (-215+7*r5)], [(275-11*r5), (-305-31*r5)]], [[(320+52*r5), (250+42*r5)], [(335+73*r5), (-15-21*r5)]], [[(145+27*r5), (140+64*r5)], [(20-60*r5), 220]]], [[[(6-2*r5), (-41+13*r5)], [(53-17*r5), (-47+11*r5)]], [[(44-8*r5), (34-6*r5)], [(41-5*r5), (3-3*r5)]], [[(19-3*r5), (8+4*r5)], [(20-12*r5), (40-12*r5)]]]], [[[[(28-4*r5), (-198+22*r5)], [(254-30*r5), (-266-6*r5)]], [[(272+24*r5), (212+20*r5)], [(278+42*r5), (-6-18*r5)]], [[(122+14*r5), (104+48*r5)], [(40-56*r5), (200-16*r5)]]], [[[(35-11*r5), (-240+71*r5)], [(310-93*r5), (-280+57*r5)]], [[(265-39*r5), (205-29*r5)], [(250-21*r5), (15-18*r5)]], [[(115-14*r5), (55+27*r5)], [(110-70*r5), (235-65*r5)]]], [[[(26-6*r5), (-181+37*r5)], [(233-49*r5), (-227+19*r5)]], [[(224-4*r5), (174-2*r5)], [(221+11*r5), (3-15*r5)]], [[(99+1*r5), (68+32*r5)], [(60-52*r5), (180-32*r5)]]], [[[(22+2*r5), (-162-20*r5)], [(206+24*r5), (-254-60*r5)]], [[(278+78*r5), (218+62*r5)], [(302+96*r5), (-24-18*r5)]], [[(128+38*r5), (146+66*r5)], [(-20-44*r5), (170+26*r5)]]]], [[[[(-27+7*r5), (187-44*r5)], [(-241+58*r5), (229-28*r5)]], [[(-223+13*r5), (-173+9*r5)], [(-217-2*r5), (-6+15*r5)]], [[(-98+3*r5), (-61-29*r5)], [(-70+54*r5), (-185+39*r5)]]], [[[(-12-4*r5), (92+32*r5)], [(-116-40*r5), (164+64*r5)]], [[(-188-76*r5), (-148-60*r5)], [(-212-88*r5), (24+12*r5)]], [[(-88-36*r5), (-116-52*r5)], [(40+24*r5), (-100-36*r5)]]], [[[(-8-4*r5), (63+31*r5)], [(-79-39*r5), (121+57*r5)]], [[(-142-66*r5), (-112-52*r5)], [(-163-75*r5), (21+9*r5)]], [[(-67-31*r5), (-94-42*r5)], [(40+16*r5), (-70-34*r5)]]], [[[(-51+11*r5), (356-67*r5)], [(-458+89*r5), (452-29*r5)]], [[(-449-1*r5), (-349-3*r5)], [(-446-31*r5), (-3+30*r5)]], [[(-199-6*r5), (-143-67*r5)], [(-110+102*r5), (-355+57*r5)]]]]]
+        >>> product.dims
+
+        :param other:
+        :return:
+        """
+        return FTensor(np.tensordot(self.components,other.components,axes=0))
 
 def generate_group():
     # generate 120 elements of the 600 cell
